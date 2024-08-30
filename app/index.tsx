815aa718406bd7d5cfe1131711609c4caa88cba1
ChiragAgg5k <chiragaggarwal5k@gmail.com>
2024-08-30 20:07:24 +0530

    finished landing page

--- app/index.tsx
+++ app/index.tsx
@@ -1,46 +1,59 @@
+import { Box } from "@/components/ui/box";
+import { Card } from "@/components/ui/card";
 import { Heading } from "@/components/ui/heading";
 import React from "react";
-import { Text, View, Image, TouchableOpacity } from "react-native";
+import { Image, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Box } from "@/components/ui/box";
 
-const Card = ({ title, image }) => (
-  <TouchableOpacity className="bg-gray-100 rounded-lg p-4 m-2 flex-1">
-    <Image
-      source={image}
-      style={{ height: 100, width: "100%", resizeMode: "contain" }}
-    />
-    <Text className="text-center mt-2 text-lg font-semibold">{title}</Text>
-  </TouchableOpacity>
-);
+const CardContainer = ({ title, image }: { title: string, image: any }) => {
+    return <Card
+        size="md" variant="outline" className="flex-1 mx-2 bg-white">
+        <Image
+            source={image}
+            className="w-full h-40"
+        />
+        <Heading size="md" className="mb-1 text-center mt-4 ">
+            {title}
+        </Heading>
+    </Card>
+}
 
 const Index = () => {
-  return (
-    <SafeAreaView className="flex-1 bg-white">
-      <Box className="px-4">
-        <Heading size="2xl" className="text-left mt-4">
-          Welcome to Inclusive Education
-        </Heading>
-        <Text className="text-left p-4 text-lg text-gray-700">
-          Learn Gujarati through Indian Sign Language
-        </Text>
-      </Box>
+    return (
+        <SafeAreaView style={{
+            paddingTop: -35,
+            paddingLeft: 15,
+            paddingRight: 15,
+        }}>
+            <Box className="mb-6">
+                <Heading size="2xl" className="text-left mt-4 mb-2">
+                    Welcome to Inclusive Education
+                </Heading>
+                <Text
+                    className="text-left font-semibold text-lg text-gray-700">
+                    Learn Gujarati through Indian Sign Language
+                </Text>
+            </Box>
 
-      <Box className=" px-4">
-        <Box className="">
-          <Card title="Alphabets" image={require("@/assets/images/icon.png")} />
-          <Card title="Numbers" image={require("@/assets/images/icon.png")} />
-        </Box>
-        <Box className="">
-          <Card title="Words" image={require("@/assets/images/icon.png")} />
-          <Card title="Sentences" image={require("@/assets/images/icon.png")} />
-        </Box>
-        <Box className="">
-          <Card title="Tables" image={require("@/assets/images/icon.png")} />
-        </Box>
-      </Box>
-    </SafeAreaView>
-  );
+
+            <Heading size="md" className="text-left mb-2">Alphabets & Numbers</Heading>
+            <View
+                className="flex flex-row justify-between"
+            >
+                <CardContainer title="Alphabets" image={require("@/assets/images/signs/alphabets.png")} />
+                <CardContainer title="Numbers" image={require("@/assets/images/signs/numbers.png")} />
+            </View>
+
+            <Heading size="md" className="text-left mt-6 mb-2">Words & Calculation</Heading>
+            <View
+                className="flex flex-row justify-between"
+            >
+                <CardContainer title="Words" image={require("@/assets/images/signs/words.png")} />
+                <CardContainer title="Calculation" image={require("@/assets/images/signs/calculation.png")} />
+            </View>
+
+        </SafeAreaView>
+    );
 };
 
 export default Index;
